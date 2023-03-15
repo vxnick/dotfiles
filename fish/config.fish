13f5c4a14d248a40b38dbf3f91285bb952d5b09e
Nick Savage <nick.savage@gohenry.co.uk>
2023-03-15 09:18:46 +0000

    Update things

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,10 @@
 set fish_greeting ""
 set -x EDITOR vim
 set -x GOPATH $HOME
+set -x CLOUDSDK_PYTHON_SITEPACKAGES 1
 
 fish_add_path $HOME/bin
 fish_add_path /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin
 
-source (brew --prefix asdf)"/asdf.fish"
 source ~/.config/fish/private.fish
 fish_add_path /usr/local/opt/mysql-client/bin
